f74f3d49190703f6bd85e92e116d18301d0cb405
Martin Brandl <martin.brandl@whiteduck.de>
2019-11-24 12:45:09 +0100

    Initial dotnet new mvc commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sample_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
