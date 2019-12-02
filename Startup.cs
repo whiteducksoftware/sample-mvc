130ee51f5f6e31506b9b3ae71db575cd650cd660
Martin Brandl <mjibrandl@gmail.com>
2019-12-02 18:14:58 +0100

    Added application insight

--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,7 @@ namespace sample_mvc
         {
             services.Configure<Settings>(Configuration);
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
