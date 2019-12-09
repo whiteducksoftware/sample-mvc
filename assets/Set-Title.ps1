ae34215fba0fd0bc7a1bb59b5b3e750e555ff1ff
Martin Brandl <mjisaak@users.noreply.github.com>
2019-12-09 17:29:29 +0100

    Update Set-Title.ps1

--- assets/Set-Title.ps1
+++ assets/Set-Title.ps1
@@ -1,5 +1,13 @@
 param(
   $title
 )
-$newTitle = "$title" -replace '.*to: '
-az webapp config appsettings set -g github-action-rg -n github-action-demo --settings title=$newTitle
+
+if ($title -match 'change title to:\s*(.+)')
+{
+    Write-Host "Changing title to $($Matches[1])"
+    az webapp config appsettings set -g github-action-rg -n github-action-demo --settings title=$($Matches[1])
+}
+else 
+{
+    Write-Host "Nothing to change."
+}
